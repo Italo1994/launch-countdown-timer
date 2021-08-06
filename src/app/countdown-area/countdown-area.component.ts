3c053cf5d5209a349bdf9998ae7256906a626573
Italo1994 <italohortiz2015@gmail.com>
2021-08-06 00:51:15 -0300

    Creating the logic of programming for the launch countdown in the file typescript

--- src/app/countdown-area/countdown-area.component.ts
+++ src/app/countdown-area/countdown-area.component.ts
@@ -8,14 +8,46 @@ import { Component, OnInit } from '@angular/core';
 export class CountdownAreaComponent implements OnInit {
 
   constructor() { 
-    let days: number = 0,
-        hours: number = 0,
-        minutes: number,
-        seconds: number;
+    let days: number = 80,
+        hours: number = 23,
+        minutes: number = 59,
+        seconds: number = 59;
 
-    function getTime() {
+    var time: number = 1000; //How many time in miliseconds is one second
+    
 
+    function start() {
+      setInterval( () => {
+        timer();
+
+      }, time);
     }
+
+    function timer() {
+      seconds--;
+       
+
+      if(seconds == 0) {
+        seconds = 59;
+        minutes--;
+
+        if(minutes == 0) {
+          minutes = 59;
+          hours--;
+
+          if(hours == 0) {
+            hours = 23;
+            days--;
+
+            if(days == 0) {
+              
+            }
+
+          }
+        }
+      }
+    }
+
   }
 
   ngOnInit(): void {
